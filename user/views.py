fbd11e21aa3a36018def0142189b7e56c902101b
masashimorita <masashi.morita.mm@gmail.com>
2022-01-29 12:37:23 +0900

    feat: :+1:  Changed to create default TodoList When user created

--- user/views.py
+++ user/views.py
@@ -1,12 +1,24 @@
+from django.db import transaction
 from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from user.serializers import UserSerializer, AuthTokenSerializer
+from core.models import TodoList
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
     serializer_class = UserSerializer
+
+    @transaction.atomic
+    def perform_create(self, serializer):
+        """Create a new user with default todo list"""
+        user = serializer.save()
+        TodoList.objects.create(
+            user=user,
+            name='Default',
+            is_default=True
+        )
 
 
 class CreateTokenView(ObtainAuthToken):
